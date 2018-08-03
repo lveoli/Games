051e7e9ef623b6d724f0cbef2018bfd66f8be189
Leo Chen <lveoli@berkeley.edu>
2018-08-02 20:11:03 -0700

    made simonSays but joseph already did it so

--- 8_2_2018_Simon_Says.py
+++ 8_2_2018_Simon_Says.py
@@ -1,15 +1,24 @@
 import time
 
-
+#starts the game by asking if the user is ready and will only proceed when yes
 def gameStart():
-	text = input("Are you ready to play Simon Says on the command line? (Yes/No): ").lower()
-	if text == "yes":
+	userInput = input("Are you ready to play Simon Says on the command line? (Yes/No): ").lower()
+	if userInput == "yes":
 		print ("Great! Let's begin")
-	elif text == "no": 
+	elif userInput == "no": 
 		print ("Okay, but let me know if you change your mind! Bye!")
 	else:
 		print ("Incorrect input please enter either 'Yes', or 'No'") 
 		gameStart()
+
+#used to prompt the user for an answer to the given pattern
+def simonSays(answer):
+	userInput = input("type in what Simon said: ")
+	if userInput == answer: 
+		print ("Congrats! You are correct!")
+		print ("Now for the next challenge:")
+	else:
+		print ("Not quite. Here, try again:")
 
 gameStart()
 
