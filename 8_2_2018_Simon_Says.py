73ec6409ded3df5132e0a5eebe0199c9ef3d7b48
Leo Chen <lveoli@berkeley.edu>
2018-08-02 19:58:14 -0700

    made the thing look fucking professional

---
+++ 8_2_2018_Simon_Says.py
@@ -0,0 +1,18 @@
+import time
+
+
+def askInput():
+	text = input("Are you ready to play Simon Says on the command line? (Yes/No): ").lower()
+	if text == "yes":
+		print ("Great! Let's begin")
+	elif text == "no": 
+		print ("Okay, but let me know if you change your mind! Bye!")
+	else:
+		print ("Incorrect input please enter either 'Yes', or 'No'") 
+		askInput()
+
+askInput()
+
+
+
+
